3ac0c4f1dbf3550720afafe7fe1576c9c7bdc0f1
Titch <Titch@Titchs-Macbook-Pro.local>
2018-06-05 13:42:58 -0700

    integrate react router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact</Link>
+          </nav>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <main>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/contact" component={Contact} />
+
+        </main>
+
+
       </div>
     );
   }
---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return(
+      <h2>Hi, I'm Richard Deane</h2>
+    )
+  }
+}
+
+export default About;
